7cc258cfe0c27efb4840ef3465099823fd862c9b
ujebb <ujebbawas@yahoo.com>
2024-06-18 07:16:18 +0700

    update function CoreConcept (destruction object)

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -20,7 +20,7 @@ function Header () {
     </header>);
 }
 
-function CoreConcept(props) {
+/*function CoreConcept(props) {
     return (
         <li>
             <img src={props.image} alt={props.title} />
@@ -28,7 +28,18 @@ function CoreConcept(props) {
             <p>{props.description}</p>
         </li>
     )
+}*/
+
+function CoreConcept({image, title, description}) {
+    return (
+        <li>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    )
 }
+
 
 function App() {
     return (
@@ -39,9 +50,12 @@ function App() {
                     <h2>Core Concepts</h2>
                     <ul>
                         <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
-                        <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image} />
+                        {/*<CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image} />
                         <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image} />
-                        <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image} />
+                        <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image} />*/}
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
                     </ul>
                 </section>
 
