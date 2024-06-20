a93197ee83ae1551e50961005b0318de69c12eb2
muhammad-huzaifah <huzaifah2009@gmail.com>
2024-06-20 17:32:43 +0700

    event onSelect on TabButton.jsx and manage state using hooks

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,8 +1,21 @@
+import {useState} from "react";
+
 import {CORE_CONCEPTS} from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton';
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
+    function handleSelect(selectedButton) {
+        // selectedButton => 'components', 'jsx', 'props', 'state
+        setSelectedTopic(selectedButton);
+        console.log(selectedTopic);
+    }
+
+    console.log('App Component Executing');
+
     return (
         <div>
             <Header />
@@ -21,6 +34,16 @@ function App() {
                     </ul>
                 </section>
 
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+                        <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+                    </menu>
+                    {selectedTopic}
+                </section>
             </main>
         </div>
     );
