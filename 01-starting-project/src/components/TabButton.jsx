e283a67d1dfadd0f16d6ab2f70e3494873dc7cc0
ujebb <ujebbawas@yahoo.com>
2024-06-29 07:43:22 +0700

    update App.jsx, update Examples.jsx, add Section.jsx, add Tabs.jsx, update TabButton.jsx

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,8 +1,9 @@
-export default function TabButton({children, onSelect, isSelected}) {
+export default function TabButton({children, isSelected, ...props}) {
     console.log('TabButton Component Executing');
     return (
     <li>
-        <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+        <button className={isSelected ? 'active' : undefined}  {...props}>{children}
+        </button>
     </li>
     );
 }
