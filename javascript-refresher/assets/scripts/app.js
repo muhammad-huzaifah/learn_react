41195f665d9ff0b5ab30fe78cfcdc071332aae3b
huzaifah <ujebbawas@yahoo.com>
2025-05-14 15:55:30 +0700

    add timeout handling functions and a greeter function

--- javascript-refresher/assets/scripts/app.js
+++ javascript-refresher/assets/scripts/app.js
@@ -232,5 +232,25 @@ console.log(output); // [{ val: 1 }, { val: 2 }, { val: 3 }]*/
 //     console.log(hobby);
 // }
 
-const list = document.querySelector("ul");
-list.remove();
+// const list = document.querySelector("ul");
+// list.remove();
+
+function handleTimeout() {
+    console.log("Timeout!");
+}
+
+const handleTimeout2 = () => {
+    console.log("Timeout... again!");
+}
+
+setTimeout(handleTimeout, 2000);
+setTimeout(handleTimeout2, 3000);
+setTimeout(()=>{
+    console.log("More timing out...");
+}, 4000)
+
+function greeter(greetFn) {
+    greetFn();
+}
+
+greeter(() => console.log("Hi"));
