7cb278c466e5f560011681c020c189df6f50bc25
huzaifah <ujebbawas@yahoo.com>
2025-05-31 17:02:18 +0700

    add random description to Header component New

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,19 +1,11 @@
-const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-    return Math.floor(Math.random() * (max+1));
-}
-
 function Header() {
-    const description = reactDescription[genRandomInt(2)];
-
     return (
         <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-            {/*{reactDescription[genRandomInt(2)]} React concepts you will need for almost any app you are going to build!*/}
-            {description} React concepts you will need for almost any app you are going to build!
+            Fundamental React concepts you will need for almost any app you are
+            going to build!
         </p>
         </header>
     );
