e5dab3cde4ae7fb5b2ec73fab24768fff5a077c9
huzaifah <ujebbawas@yahoo.com>
2025-07-14 19:13:55 +0700

    wrap App JSX with a fragment to simplify structure and remove unnecessary div

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -32,27 +32,27 @@ function App() {
     }
 
     return (
-        <div>
+        <>
             <Header />
-            <main>
-                <section id="core-concepts">
-                    <h2>Core Concepts</h2>
-                    <ul>
-                        {CORE_CONCEPTS.map((conceptItem) => (<CoreConcepts key={conceptItem.title} {...conceptItem}/>))}
-                    </ul>
-                </section>
-                <section id="examples">
-                    <h2>Examples</h2>
-                    <menu>
-                        <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect ('components')}>Components</TabButton>
-                        <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect ('jsx')}>JSX</TabButton>
-                        <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect ('props')}>Props</TabButton>
-                        <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect ('state')}>State</TabButton>
-                    </menu>
-                    {tabContent}
-                </section>
-            </main>
-        </div>
+                <main>
+                    <section id="core-concepts">
+                        <h2>Core Concepts</h2>
+                        <ul>
+                            {CORE_CONCEPTS.map((conceptItem) => (<CoreConcepts key={conceptItem.title} {...conceptItem}/>))}
+                        </ul>
+                    </section>
+                    <section id="examples">
+                        <h2>Examples</h2>
+                        <menu>
+                            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect ('components')}>Components</TabButton>
+                            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect ('jsx')}>JSX</TabButton>
+                            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect ('props')}>Props</TabButton>
+                            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect ('state')}>State</TabButton>
+                        </menu>
+                        {tabContent}
+                    </section>
+                </main>
+        </>
     );
 }
 
