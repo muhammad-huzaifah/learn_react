c4fcf858421a190539c348492b5ca034418af6a3
huzaifah <ujebbawas@yahoo.com>
2025-07-23 04:29:45 +0700

    refactor CoreConcepts and Examples components for improved readability, consistency, and maintainability

--- 01-starting-project/src/components/CoreConcepts.jsx
+++ 01-starting-project/src/components/CoreConcepts.jsx
@@ -1,21 +1,13 @@
-import CoreConcept from "./CoreConcept.jsx"
-import { CORE_CONCEPTS } from "../data.js"
+import CoreConcept from "./CoreConcept.jsx";
+import {CORE_CONCEPTS} from '../data.js';
 
-export default function Coreconcepts () {
+export default function CoreConcepts() {
     return (
         <section id="core-concepts">
-                    <h2>Core Concepts</h2>
-                    <ul>
-                        {CORE_CONCEPTS.map((conceptItem) => (<CoreConcept key={conceptItem.title} {...conceptItem} /> ) )}
-                        {/*<CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} images={CORE_CONCEPTS[0].images} />*/}
-                        {/*<CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} images={CORE_CONCEPTS[1].images} />
-                        <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} images={CORE_CONCEPTS[2].images} />
-                        <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} images={CORE_CONCEPTS[3].images} />*/}
-                        {/*<CoreConcept {...CORE_CONCEPTS[0]} />
-                        <CoreConcept {...CORE_CONCEPTS[1]} />
-                        <CoreConcept {...CORE_CONCEPTS[2]} />
-                        <CoreConcept {...CORE_CONCEPTS[3]} />*/}
-                    </ul>
-                </section>
-    )
+            <h2>Core Concepts</h2>
+            <ul>
+                {CORE_CONCEPTS.map((conceptItem) => (<CoreConcept key={conceptItem.title} {...conceptItem}/>))}
+            </ul>
+        </section>
+    );
 }
