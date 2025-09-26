0529c4b900240d5619030b9e8be59add58fe80df
muhammad-huzaifah <huzaifah2009@gmail.com>
2025-09-27 04:01:42 +0700

    refactor: move game board initialization logic from GameBoard to App and update prop handling for clarity

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -15,11 +15,34 @@ function deriveActivePlayer(gameTurns) {
     return currentPlayer;
 }
 
+const initialGameBoard =  [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+  // const [hasWinner, setHasWinner] = useState(false);
   // const [activePlayer, setActivePlayer] = useState('X');
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+    let gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+        gameBoard[row][col] = player;
+    }
+
+    for (const combination of WINNING_COMBINATIONS) {
+        const firstSquareSymbol
+        const secondSquareSymbol
+        const thirdSquareSymbol
+
+
+    }
 
   function handleSelectSquare(rowIndex, colIndex) {
       // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
@@ -40,10 +63,7 @@ function App() {
             <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
             <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
             </ol>
-            <GameBoard
-                onSelectSquare={handleSelectSquare}
-                turns={gameTurns}
-            />
+            <GameBoard onSelectSquare={handleSelectSquare} boards={gameBoard} />
         </div>
         <Log turns={gameTurns}/>
     </main>
--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,31 +1,4 @@
-const initialGameBoard =  [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
-
-export default function GameBoard({onSelectSquare, turns}) {
-    let gameBoard = initialGameBoard;
-
-    for (const turn of turns) {
-        const {square, player} = turn;
-        const {row, col} = square;
-
-        gameBoard[row][col] = player;
-    }
-
-    /*const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevBoard) => {
-            const updatedBoard = [...prevBoard.map(innerArray => [...innerArray])];
-            updatedBoard [rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        })
-
-        onSelectSquare();
-    }*/
-
+export default function GameBoard({onSelectSquare, boards}) {
     return (
         <ol id="game-board">
             { gameBoard.map((row, rowIndex) => (
