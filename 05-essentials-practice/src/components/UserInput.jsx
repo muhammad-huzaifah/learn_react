e2586ced3592e77211a128cacac1182336739002
muhammad-huzaifah <huzaifah2009@yahoo.com>
2025-10-24 20:14:46 +0700

    Integrate `UserInput` component into `App` and implement input handling with `useState`.

--- 05-essentials-practice/src/components/UserInput.jsx
+++ 05-essentials-practice/src/components/UserInput.jsx
@@ -8,23 +8,32 @@ export default function UserInput() {
         duration: 10,
     });
 
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserState => {
+            return {
+                ...prevUserState,
+                [inputIdentifier]: newValue,
+            };
+        });
+    }
+
     return <section id="user-input">
         <div className="input-group">
             <p>
                 <label>Initial Investment</label>
-                <input type="number" required />
+                <input type="number" required value={userInput.initialInvestment} onChange={() => handleChange('initialInvestment', event.target.value)} />
             </p>
             <p>
                 <label>Annual Investment</label>
-                <input type="number" required />
+                <input type="number" required value={userInput.annualInvestment} onChange={() => handleChange('annualInvestment', event.target.value)} />
             </p>
             <p>
                 <label>Expected Return</label>
-                <input type="number" required/>
+                <input type="number" required value={userInput.expectedReturn} onChange={() => handleChange('expectedReturn', event.target.value)}/>
             </p>
             <p>
                 <label>Duration</label>
-                <input type="number" required/>
+                <input type="number" required value={userInput.duration} onChange={() => handleChange('duration', event.target.value)}/>
             </p>
         </div>
     </section>
