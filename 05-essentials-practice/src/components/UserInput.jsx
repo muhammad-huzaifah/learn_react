6f1b9a74b20e113ecaa9ac2abfd0dbc0d61e2e81
muhammad-huzaifah <huzaifah2009@yahoo.com>
2025-10-24 08:06:19 +0700

    - Update ESLint and related dependencies to latest versions in `package-lock.json`. - Add initial state management to `UserInput` component using `useState`. - Enhance PHP configurations with PhpCodeSniffer, PhpStan, and Psalm in `.idea/php.xml`.

--- 05-essentials-practice/src/components/UserInput.jsx
+++ 05-essentials-practice/src/components/UserInput.jsx
@@ -1,4 +1,13 @@
+import {useState} from 'react';
+
 export default function UserInput() {
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
     return <section id="user-input">
         <div className="input-group">
             <p>
