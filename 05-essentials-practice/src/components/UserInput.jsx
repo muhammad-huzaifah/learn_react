b60cc2019d835d77935a77bffbceb54fd1ccb918
muhammad-huzaifah <huzaifah2009@gmail.com>
2025-10-25 18:24:21 +0700

    fix: pass event object to onChange handlers in UserInput component

--- 05-essentials-practice/src/components/UserInput.jsx
+++ 05-essentials-practice/src/components/UserInput.jsx
@@ -21,19 +21,19 @@ export default function UserInput() {
         <div className="input-group">
             <p>
                 <label>Initial Investment</label>
-                <input type="number" required value={userInput.initialInvestment} onChange={() => handleChange('initialInvestment', event.target.value)} />
+                <input type="number" required  value={userInput.initialInvestment} onChange={(event) => handleChange('initialInvestment', event.target.value)} />
             </p>
             <p>
                 <label>Annual Investment</label>
-                <input type="number" required value={userInput.annualInvestment} onChange={() => handleChange('annualInvestment', event.target.value)} />
+                <input type="number" required value={userInput.annualInvestment} onChange={(event) => handleChange('annualInvestment', event.target.value)} />
             </p>
             <p>
                 <label>Expected Return</label>
-                <input type="number" required value={userInput.expectedReturn} onChange={() => handleChange('expectedReturn', event.target.value)}/>
+                <input type="number" required value={userInput.expectedReturn} onChange={(event) => handleChange('expectedReturn', event.target.value)}/>
             </p>
             <p>
                 <label>Duration</label>
-                <input type="number" required value={userInput.duration} onChange={() => handleChange('duration', event.target.value)}/>
+                <input type="number" required value={userInput.duration} onChange={(event) => handleChange('duration', event.target.value)}/>
             </p>
         </div>
     </section>
