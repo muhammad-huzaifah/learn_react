b95fe47cfb17c49a84b5ea84e343f23d49abc7a4
muhammad-huzaifah <huzaifah2009@gmail.com>
2025-11-09 08:01:03 +0700

    fix: add validation for duration input and conditional rendering of Results component

--- 05-essentials-practice/src/App.jsx
+++ 05-essentials-practice/src/App.jsx
@@ -12,6 +12,8 @@ function App() {
         duration: 10,
     });
 
+    const inputIsValid = userInput.duration >=1;
+
     function handleChange(inputIdentifier, newValue) {
         setUserInput(prevUserState => {
             return {
@@ -25,7 +27,10 @@ function App() {
     <>
         <Header userInput={userInput}/>
         <UserInput userInput={userInput} onChange={handleChange} />
-        <Results input={userInput} />
+        {!inputIsValid && (
+            <p className="center">Please enter a duration greater than zero.</p>
+        )}
+        {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
