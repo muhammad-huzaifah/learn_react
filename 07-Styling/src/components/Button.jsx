3cbcf7918b227d820dc4786e609dc0f7c52c6ef7
muhammad-huzaifah <huzaifah2009@gmail.com>
2025-12-20 08:16:57 +0700

    refactor: update Button styles and remove unused CSS rules in "07-Styling" module

--- 07-Styling/src/components/Button.jsx
+++ 07-Styling/src/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button ({ children, ...props }) {
     return (
-        <button className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-950" {...props}
+        <button className="bg-amber-400 hover:bg-amber-700 px-4 py-2 font-semibold uppercase rounded text-stone-900 " {...props}
         >
          {children}
         </button>
