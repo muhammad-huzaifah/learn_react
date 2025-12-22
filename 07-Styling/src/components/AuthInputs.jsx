a5f686815949efa0fbb1ed7a0e2ea6e20a8dcc1f
muhammad-huzaifah <huzaifah2009@gmail.com>
2025-12-22 20:15:03 +0700

    refactor: update AuthInputs layout and buttons with improved Tailwind styling

--- 07-Styling/src/components/AuthInputs.jsx
+++ 07-Styling/src/components/AuthInputs.jsx
@@ -25,7 +25,7 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-linear-to-b from-stone-700 to-stone-800">
-      <div>
+      <div className="flex flex-col gap-2 mb-6">
           <Input
             label="Email"
             invalid = {emailNotValid}
@@ -45,8 +45,8 @@ export default function AuthInputs() {
           />
 
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
