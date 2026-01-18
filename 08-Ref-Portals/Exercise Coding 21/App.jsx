ef92d730b51f55cba2386c05eba597303a2b8e53
muhammad-huzaifah <huzaifah2009@gmail.com>
2026-01-18 11:41:17 +0700

    feat: add file picker UI with styling for "Exercise Coding 21" in "08-Ref-Portals" module

---
+++ 08-Ref-Portals/Exercise Coding 21/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <div id="app">
+            <p>Please select an image</p>
+            <p>
+                <input data-testid="file-picker" type="file" accept="image/*" />
+                <button>Pick Image</button>
+            </p>
+        </div>
+    );
+}
+
+export default App;
