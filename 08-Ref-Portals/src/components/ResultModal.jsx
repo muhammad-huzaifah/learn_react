a7527efcc382ec871c37af7bb830763acd2b1cf7
muhammad-huzaifah <huzaifah2009@gmail.com>
2026-02-23 08:52:48 +0700

    refactor: forward `ref` to ResultModal and integrate dialog refs in TimerChallenge

--- 08-Ref-Portals/src/components/ResultModal.jsx
+++ 08-Ref-Portals/src/components/ResultModal.jsx
@@ -1,6 +1,8 @@
-export default function ResultModal({result, targetTime}) {
+import {forwardRef} from 'react';
+
+const ResultModal = forwardRef(function ResultModal ({result, targetTime}, ref) {
     return (
-        <dialog className="result-modal" open>
+        <dialog ref={ref} className="result-modal" open>
             <h2>You {result}</h2>
             <p>
                 The target time was <strong>{targetTime} seconds.</strong>
@@ -13,4 +15,4 @@ export default function ResultModal({result, targetTime}) {
             </form>
         </dialog>
     );
-}
+});
--- 08-Ref-Portals/src/components/TimerChallenge.jsx
+++ 08-Ref-Portals/src/components/TimerChallenge.jsx
@@ -5,15 +5,17 @@ import ResultModal from "./ResultModal.jsx";
 
 export default function timerChallenge ({ title, targetTime}) {
     const timer = useRef();
+    const dialog = useRef();
 
     const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
 
     function handleStart() {
-        timer.current = setTimeout(() => setTimerExpired(true),
-            targetTime * 1000);
-
-     setTimerStarted(true);
+        timer.current = setTimeout(() => {
+            setTimerExpired(true);
+            dialog.current.showModal
+            },targetTime * 1000);
+        setTimerStarted(true);
   }
 
   function handleStop() {
@@ -22,7 +24,7 @@ export default function timerChallenge ({ title, targetTime}) {
 
     return (
         <>
-            {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
+            {timerExpired && <ResultModal ref={dialog} targetTime={targetTime} result="lost" />}
             <section className="challenge">
                 <h2>{title}</h2>
                 {timerExpired && <p>You lost!</p>}
